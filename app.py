073f94317732810ee5f7bd1e201e59732687a4f1
SASHWAT ROY <141540184+Sawarn01@users.noreply.github.com>
2024-12-04 23:23:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ import io
 from fpdf import FPDF
 app = Flask(__name__)
 
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
